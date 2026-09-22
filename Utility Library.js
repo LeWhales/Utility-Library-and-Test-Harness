bee2e35f8842bbcb4c1010d4a8ef81ffc34bb72f
LeWhales <bigwhales90@gmail.com>
2026-09-22 15:56:48 -0500

    Add files via upload

---
+++ Utility Library.js
@@ -0,0 +1,48 @@
+function Validation(True, False) {
+    let isvalid = True === 'true'
+    let isinvalid = False === 'false'
+
+    let ValidationCheck = (isinvalid || isvalid);
+
+    if (!ValidationCheck) {
+        return {
+            valid: "cant confirm",
+        };
+    }
+    if (isinvalid) {
+        return {
+            Allowed: false,
+            Valid: "false"
+
+        }
+    }
+    if (isvalid) {
+        return {
+            Allowed: true,
+            Valid: "true"
+        }
+    };
+};
+function Fee(Pay, Upkeep = 0.3) {
+    const FinalPay = Pay * Upkeep
+    return FinalPay
+};
+function Duration(OneDay, OneWeek, OneMonth) {
+    let isOneDay = (OneDay = 1);
+    let isWeek = (OneWeek >= 7 || OneWeek <= 7);
+    let isMonth = (OneMonth >= 31);
+
+    let DurationCheck = (isOneDay || isWeek || isMonth)
+
+
+};
+function Capacity(single, small, medium) {
+
+};
+
+function Label(Fiction, NonFiction, Fantasy, ScienceFiction) { };
+
+function Summary(send) { send = Validation, Fee, Duration, Capacity, Label };
+
+
+console.log(Summary(true), Fee(10));
